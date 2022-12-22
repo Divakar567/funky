a80116e6c7734316e58857dcbd1bfb4df5c4f2b4
Divakar Babu Budumuru <divakar.budumuru567@gmail.com>
2022-12-23 02:36:34 +0530

    project set up completed

---
+++ src/main/java/com/diva/funky/FunkyApplication.java
@@ -0,0 +1,13 @@
+package com.diva.funky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunkyApplication.class, args);
+	}
+
+}
